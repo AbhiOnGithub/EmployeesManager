524c2c8585770d503c97340cb88be8f47c2562da
Abhishek <innovative.abhishek@hotmail.com>
2018-07-09 04:23:23 +0530

    First Commit

---
+++ ReactCrudDemo/Migrations/20180708221057_BaseMigration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ReactCrudDemo.Migrations
+{
+    public partial class BaseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblCities",
+                columns: table => new
+                {
+                    CityID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CityName = table.Column<string>(unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCities", x => x.CityID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblEmployee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(unicode: false, maxLength: 20, nullable: false),
+                    Department = table.Column<string>(unicode: false, maxLength: 20, nullable: false),
+                    Gender = table.Column<string>(unicode: false, maxLength: 6, nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblEmployee", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblCities");
+
+            migrationBuilder.DropTable(
+                name: "tblEmployee");
+        }
+    }
+}
